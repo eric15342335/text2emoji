63eb76699da3970bf51b92e63aa2aa32ddf359b2
Cheng Ho Ming <eric310@connect.hku.hk>
2024-02-25 22:09:40 +0800

    Support for numbers
    Co-authored-by: 84ds84d8s <129842660+84ds84d8s@users.noreply.github.com>

--- text2emoji.py
+++ text2emoji.py
@@ -1,8 +1,20 @@
+# the dictionary that stores the character and their emoji counterpart
 convert_dict = {}
+
+# ASCII 'A' is 65
 counter = 65
 for emojis in "🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿".split():
     convert_dict[chr(counter)] = emojis
+    # 'B' is 66, 'C' is 67 etc
+    counter += 1
+
+# numerical emojis
+counter = 0
+for emojis in "⿠ ⿡ ⿢ ⿣ ⿤ ⿥ ⿦ ⿧ ⿨ ⿩".split():
+    convert_dict[counter] = emojis
     counter += 1
 
 for character in input():
+    # use the dict.get() method since a lot of characters not included in the convert_dict
     print(convert_dict.get(character.upper(), character), end=" ")
+    input("Press any key to exit . . .")
