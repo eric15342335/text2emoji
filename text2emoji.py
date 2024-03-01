572c0bafb0cd43eda12e89c1b9dfaff89698954c
Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>
2024-03-01 14:25:59 +0800

    Reduce file sixe of text2emoji.py while retaining all functions
    Signed-off-by: Chu Chun Yin, Jeremy  <129842660+84ds84d8s@users.noreply.github.com>

--- text2emoji.py
+++ text2emoji.py
@@ -1,52 +1,20 @@
-"""Convert characters to their emoji counterparts."""
-
-
 def main():
-    """Initialize the conversion dictionary and wait for user input"""
-    # the dictionary that stores the character and their emoji counterpart
-    convert_dict = {}
-
-    # ASCII 'A' is 65
-    counter = 65
-    for emojis in "🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿".split():
-        convert_dict[chr(counter)] = emojis
-        # 'B' is 66, 'C' is 67 etc
-        counter += 1
-
-    # numerical emojis
-    counter = 48
-    for emojis in "⿠ ⿡ ⿢ ⿣ ⿤ ⿥ ⿦ ⿧ ⿨ ⿩".split():
-        # note that chr(48) means '0' which is a character
-        convert_dict[chr(counter)] = emojis
-        counter += 1
-
-    # other emojis
-    convert_dict["!"] = "❗"
-
-    counter = 61
-    for emojis in "🟰 ❓".split():
-        convert_dict[chr(counter)] = emojis
-        counter += 2
-
-    counter = 43
-    for emojis in "➕ ➖".split():
-        convert_dict[chr(counter)] = emojis
-        counter += 2
-
-    counter = 158
-    for emojis in "✖ ➗".split():
-        convert_dict[chr(counter)] = emojis
-        counter += 88  # 158+88 = 246 is the ascii for division
-
-    while True:
-        # reading users input and print out the converted characters
-        for character in input():
-            # use the dict.get() method since a lot of characters not included in the convert_dict
-            print(convert_dict.get(character.upper(), character), end=" ")
-
-        # input() needs an \n since we print stuff with argument end=" " instead of end="\n"
-        if input("\nPress 1 to continue, any other key to exit: ") != "1":
-            break
-
-
+    string=input()
+    lib1="❗eeeeeeeee➕e➖ee⿠⿡⿢⿣⿤⿥⿦⿧⿨⿩eee🟰e❓e🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶🇷🇸🇹🇺🇻🇼🇽🇾🇿eeeeee🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶🇷🇸🇹🇺🇻🇼🇽🇾🇿"
+    for char in string:
+        if ord(char)<158:
+            x=lib1[ord(char)-33]#as 33 char !in lib1
+            if x!="e":
+                print(x, end=" ")
+            else:#when char unincluded
+                print(char, end=" ")
+        elif ord(char)==158:
+            print("✖", end=" ")
+        elif ord(char)==246:
+            print("➗", end=' ')
+        else:
+            print(char, end=" ")
+    u = input("Press 1 to continue, any other key to exit: ")
+    if u == "1":
+        main()
 main()
